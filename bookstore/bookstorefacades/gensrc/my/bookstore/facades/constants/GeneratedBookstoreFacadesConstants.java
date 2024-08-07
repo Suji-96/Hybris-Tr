be9cf3f24ffc89b767e85380f7602f54377f41d6
Nayak <sujayani.nayak@accenture.com>
2024-08-07 10:47:14 +0530

    first commit

---
+++ bookstore/bookstorefacades/gensrc/my/bookstore/facades/constants/GeneratedBookstoreFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Aug 3, 2024, 5:38:40 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package my.bookstore.facades.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedBookstoreFacadesConstants
+{
+	public static final String EXTENSIONNAME = "bookstorefacades";
+	
+	protected GeneratedBookstoreFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ bookstore/bookstoreinitialdata/gensrc/my/bookstore/initialdata/constants/GeneratedBookstoreInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Aug 3, 2024, 5:38:40 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package my.bookstore.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedBookstoreInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "bookstoreinitialdata";
+	
+	protected GeneratedBookstoreInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
